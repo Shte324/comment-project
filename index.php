a52c75d76cce585412703564f98859c9d5be7233
Ste324 <gregory24_7@mail.ru>
2025-05-14 10:55:03 +0500

    Добавил меню и ссылки

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php 
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    <?php 
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $author  = htmlspecialchars($_POST['author'] ?? '');
@@ -12,5 +20,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     }else{
         echo "Необходимо заполнить все поля";
     }
-    //git на кухне фаворит
-}
+ }
+  ?>
+ </body>
+ </html>
